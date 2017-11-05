70698a03407a33479fdefce428f828be6a0eb093
Kimak <mickael.dumand@gmail.com>
2017-11-06 00:02:49 +0100

    add simple StackNavigator

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
 import React from "react";
 import { StatusBar } from "react-native";
 import Home from "./src/features/home";
+import Places from "./src/features/places";
 StatusBar.setHidden(true);
 
-const App = () => <Home />;
-export default App;
+import { StackNavigator } from "react-navigation";
+
+const RootNavigator = StackNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      header: null
+    }
+  },
+  Places: {
+    screen: Places,
+    navigationOptions: {
+      header: null
+    }
+  }
+});
+
+export default RootNavigator;
--- src/features/home/index.js
+++ src/features/home/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import Button from "../../shared-ui/Button";
 
-const Home = () => (
+const Home = ({ navigation }) => (
   <View>
     <Text>Home page</Text>
-    <Button label="Navigate >" onPress={() => {}} />
+    <Button label="Navigate >" onPress={() => navigation.navigate("Places")} />
   </View>
 );
 
