0fffdf4555db72fc23c6144557406cad5a6ad629
Kimak <mickael.dumand@gmail.com>
2017-11-05 23:54:19 +0100

    clean names

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { StatusBar } from "react-native";
-import Places from "./src/features/home";
+import Home from "./src/features/home";
 StatusBar.setHidden(true);
 
-const App = () => <Places />;
-
+const App = () => <Home />;
 export default App;
--- src/features/places/index.js
+++ src/features/places/index.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
   }
 });
 
-export default class App extends Component {
+export default class Places extends Component {
   constructor() {
     super();
     this.state = {
