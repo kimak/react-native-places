4512aae690d991609ddcd33413a24c39e1b4747a
Kimak <mickael.dumand@gmail.com>
2017-11-14 12:01:30 +0100

    add redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,9 @@ import places from "./reducers/places";
 import placesFilter from "./reducers/placesFilter";
 
 let reducer = combineReducers({ places, placesFilter: placesFilter });
-let store = createStore(reducer);
+let store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
