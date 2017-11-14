5b141faba871a5df215f175fd73f15ff5e4656af
Kimak <mickael.dumand@gmail.com>
2017-11-14 11:01:35 +0100

    fix initialStates

--- src/redux/reducers/places.js
+++ src/redux/reducers/places.js
@@ -1,60 +1,57 @@
-const initialState = {
-  filter: "all",
-  places: [
-    {
-      id: 0,
-      label: "Hong Kong",
-      visited: true
-    },
-    {
-      id: 1,
-      label: "Londres",
-      visited: true
-    },
-    {
-      id: 2,
-      label: "Singapour",
-      visited: true
-    },
-    {
-      id: 3,
-      label: "Bangkok",
-      visited: true
-    },
-    {
-      id: 4,
-      label: "Paris"
-    },
-    {
-      id: 5,
-      label: "Macao"
-    },
-    {
-      id: 6,
-      label: "Dubai"
-    },
-    {
-      id: 7,
-      label: "Shenzhen"
-    },
-    {
-      id: 8,
-      label: "New York"
-    },
-    {
-      id: 9,
-      label: "Istanbul"
-    },
-    {
-      id: 10,
-      label: "Kuala Lumpur"
-    },
-    {
-      id: 11,
-      label: "Antalya"
-    }
-  ]
-};
+const initialState = [
+  {
+    id: 0,
+    label: "Hong Kong",
+    visited: true
+  },
+  {
+    id: 1,
+    label: "Londres",
+    visited: true
+  },
+  {
+    id: 2,
+    label: "Singapour",
+    visited: true
+  },
+  {
+    id: 3,
+    label: "Bangkok",
+    visited: true
+  },
+  {
+    id: 4,
+    label: "Paris"
+  },
+  {
+    id: 5,
+    label: "Macao"
+  },
+  {
+    id: 6,
+    label: "Dubai"
+  },
+  {
+    id: 7,
+    label: "Shenzhen"
+  },
+  {
+    id: 8,
+    label: "New York"
+  },
+  {
+    id: 9,
+    label: "Istanbul"
+  },
+  {
+    id: 10,
+    label: "Kuala Lumpur"
+  },
+  {
+    id: 11,
+    label: "Antalya"
+  }
+];
 
 function places(state = initialState, action) {
   switch (action.type) {
