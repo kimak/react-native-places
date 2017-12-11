813f79c0103617c29f00a58fe06f4ae97c4edfdb
Kimak <mickael.dumand@gmail.com>
2017-12-11 13:04:59 +0100

    add react-native-svg module

--- src/features/home/index.js
+++ src/features/home/index.js
@@ -1,24 +1,25 @@
 import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import Button from "../../shared-ui/Button";
+import Svg, { Circle } from "react-native-svg";
 
 const styles = StyleSheet.create({
   root: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1
   },
   titleBold: {
     fontFamily: "Poppins-Bold",
-    fontSize: 32,
+    fontSize: 32
   },
   titleRegular: {
     fontFamily: "Poppins-Regular",
-    fontSize: 32,
+    fontSize: 32
   },
   titleThin: {
     fontFamily: "Poppins-Thin",
-    fontSize: 32,
+    fontSize: 32
   }
 });
 
@@ -27,6 +28,9 @@ const Home = ({ navigation }) => (
     <Text style={styles.titleBold}>Hello</Text>
     <Text style={styles.titleRegular}>Poppins</Text>
     <Text style={styles.titleThin}>Fonts</Text>
+    <Svg height="100" width="100">
+      <Circle cx="50" cy="50" r="50" fill="pink" />
+    </Svg>
     <Button label="Navigate >" onPress={() => navigation.navigate("Places")} />
   </View>
 );
