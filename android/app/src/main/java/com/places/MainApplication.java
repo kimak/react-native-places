6e2281d95c80d420fdf17aecdad4706b916ada71
Kimak <mickael.dumand@gmail.com>
2017-12-14 15:31:13 +0100

    add react-native-lottie

--- android/app/src/main/java/com/places/MainApplication.java
+++ android/app/src/main/java/com/places/MainApplication.java
@@ -3,6 +3,7 @@ package com.places;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LottiePackage(),
             new MapsPackage(),
             new SvgPackage()
       );
--- src/features/home/index.js
+++ src/features/home/index.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Animated
+} from "react-native";
 import Button from "../../shared-ui/Button";
 import Icon from "../../shared-ui/Icon";
 import Svg, { Circle } from "react-native-svg";
 import Swiper from "react-native-swiper";
+import LottieView from "lottie-react-native";
+import animationExport from "../../assets/animations/shape_types.json";
 
 const styles = StyleSheet.create({
   root: {},
@@ -38,29 +46,53 @@ const styles = StyleSheet.create({
   }
 });
 
-const Home = ({ navigation }) => (
-  <Swiper style={styles.root} showsButtons={false}>
-    <View style={styles.slide1}>
-      <Text style={styles.titleBold}>Welcome</Text>
-      <Text style={styles.titleRegular}>on</Text>
-      <Text style={styles.titleThin}>Places</Text>
-      <Button label="Start >" onPress={() => navigation.navigate("Places")} />
-    </View>
-    <View style={styles.slide2}>
-      <Svg height="100" width="100">
-        <Circle cx="50" cy="50" r="50" fill="#007aff" />
-      </Svg>
-      <Text style={styles.titleBold}>Save your </Text>
-      <Text style={styles.titleRegular}>favorite</Text>
-      <Text style={styles.titleBold}>Place !</Text>
-    </View>
-    <View style={styles.slide3}>
-      <Text style={styles.titleBold}>Share it</Text>
-      <Text style={styles.titleRegular}>with your friends</Text>
-      <Icon type="homer" />
-      <Button label="Enjoy !" onPress={() => navigation.navigate("Places")} />
-    </View>
-  </Swiper>
-);
+let progress = new Animated.Value(0);
+class Home extends React.Component {
+  loop() {
+    Animated.timing(progress, {
+      toValue: 1,
+      duration: 5000
+    }).start(({ finished }) => {
+      if (finished) {
+        progress.setValue(0);
+        this.loop();
+      }
+    });
+  }
+  componentDidMount() {
+    this.loop();
+  }
+  render = () => {
+    const { navigation } = this.props;
+    return (
+      <Swiper style={styles.root} showsButtons={false}>
+        <View style={styles.slide1}>
+          <LottieView progress={progress} source={animationExport} />
+          <Text style={styles.titleBold}>Welcome</Text>
+          <Text style={styles.titleRegular}>on</Text>
+          <Text style={styles.titleThin}>Places</Text>
+          <Button
+            label="Start >"
+            onPress={() => navigation.navigate("Places")}
+          />
+        </View>
+        <View style={styles.slide2}>
+          <Text style={styles.titleBold}>Save your </Text>
+          <Text style={styles.titleRegular}>favorite</Text>
+          <Text style={styles.titleBold}>Place !</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.titleBold}>Share it</Text>
+          <Text style={styles.titleRegular}>with your friends</Text>
+          <Icon type="homer" />
+          <Button
+            label="Enjoy !"
+            onPress={() => navigation.navigate("Places")}
+          />
+        </View>
+      </Swiper>
+    );
+  };
+}
 
 export default Home;
